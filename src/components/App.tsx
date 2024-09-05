b427dc4253ba0566196346c2ccbfc2117d4b7a02
Logan Terry <randoom97@live.com>
2024-09-05 10:17:09 -0600

    fix issue with shaders having white backgrounds

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,8 +11,9 @@ export function App() {
 
   useEffect(() => {
     setShaderIds([]);
-    setShaderIds(["XltGRX", "MsKcRh", "XcfBRn", "XffBzl", "Xds3zN"]);
+    // setShaderIds(["XltGRX", "MsKcRh", "XcfBRn", "XffBzl", "Xds3zN"]);
     // setShaderIds(["4dcGW2"]); // this one makes use of a buffer it never set up
+    setShaderIds(["tlVGDt", "tsXBzS"]); // problem with white backgrounds
     // getShaders({ sortBy, pageSize: 12, pageNumber: 0 }).then((shaders) =>
     //   setShaderIds(shaders.Results)
     // );
